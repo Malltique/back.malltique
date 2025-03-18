e580fce90b3f4c360c638081b0b2d466343a43c1
alev <VYGrigoryan@alfabank.ru>
2025-03-18 10:58:14 +0400

    tests and fixes for one category

--- back/src/db/sqlite3/entities/Categories/categories.test.ts
+++ back/src/db/sqlite3/entities/Categories/categories.test.ts
@@ -1,15 +1,16 @@
 import { DELETE_ALL_CATEGORIES, INITIALIZE_CATEGORIES_TABLE } from "./sql";
 import { AllCategories } from "./repositories/AllCategories";
 import { generateInMemoryDb } from "../../utils";
+import { Category } from "./item";
 
 const inMemoryDb = generateInMemoryDb();
 
-beforeAll(() => {
-    inMemoryDb.execute([INITIALIZE_CATEGORIES_TABLE, undefined]);
-});
+beforeAll(() => {});
 
-beforeEach(() => {
-    inMemoryDb.execute(DELETE_ALL_CATEGORIES);
+beforeEach(async () => {
+    await inMemoryDb.execute(["DROP TABLE IF EXISTS Categories", undefined]);
+    await inMemoryDb.execute([INITIALIZE_CATEGORIES_TABLE, undefined]);
+    // await inMemoryDb.execute(DELETE_ALL_CATEGORIES);
 });
 
 test("All categories repository", async () => {
@@ -25,4 +26,18 @@ test("All categories repository", async () => {
     await allCategories.deleteAll();
     const afterDeletion = await allCategories.model();
     expect(afterDeletion).toMatchObject([]);
+});
+
+test("One category by id", async () => {
+    const allCategories = new AllCategories(inMemoryDb);
+    await allCategories.create([{ name: "electronics" }]);
+    const category = new Category(inMemoryDb, 1);
+    const beforeModify = await category.model();
+    expect(beforeModify).toMatchObject({ id: 1, name: "electronics" });
+    await category.modify({ name: "foods" });
+    const afterModify = await category.model();
+    expect(afterModify).toMatchObject({ id: 1, name: "foods" });
+    await category.delete();
+    const emptyCategories = await allCategories.model();
+    expect(emptyCategories).toMatchObject([]);
 });
--- back/src/db/sqlite3/entities/Categories/sql.ts
+++ back/src/db/sqlite3/entities/Categories/sql.ts
@@ -9,17 +9,20 @@ export const INITIALIZE_CATEGORIES_TABLE = `
   )`;
 
 export const SELECT_CATEGORY_BY_ID = (id: number) =>
-    ["SELECT * FROM Categories WHERE id = :id", { id }] as const;
+    ["SELECT * FROM Categories WHERE id = ?", [id]] as const;
 
 export const SELECT_ALL_CATEGORIES = [
     "SELECT * FROM Categories",
     undefined,
 ] as const;
 
-export const UPDATE_CATEGORY_BY_ID = (id: number, payload: object) =>
+export const UPDATE_CATEGORY_BY_ID = (
+    id: number,
+    payload: Record<string, any>
+) =>
     [
         `UPDATE Categories SET ${updatePayload(payload)} WHERE id = :id`,
-        { id, ...payload },
+        { ":id": id, ":name": payload["name"] },
     ] as const;
 
 export const INSERT_CATEGORIES = (payload: CreatePayload<CategoryDTO>[]) =>
@@ -29,7 +32,7 @@ export const INSERT_CATEGORIES = (payload: CreatePayload<CategoryDTO>[]) =>
     ] as const;
 
 export const DELETE_CATEGORY_BY_ID = (id: number) =>
-    ["DELETE FROM Categories WHERE id = :id", { id }] as const;
+    ["DELETE FROM Categories WHERE id = ?", [id]] as const;
 
 export const DELETE_ALL_CATEGORIES = [
     "DELETE FROM CATEGORIES",
