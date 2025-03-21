e8d5f4227a25dbcc9d54eca580e3eb6a71a92db4
alev <VYGrigoryan@alfabank.ru>
2025-03-21 13:19:13 +0400

    removed unused types

--- back/src/api/controllers/Roles/index.ts
+++ back/src/api/controllers/Roles/index.ts
@@ -1,4 +1,4 @@
-import { Express, Request, Response } from "express";
+import { Express } from "express";
 import { buildURL } from "../../utils";
 import { authMiddleware, SECRET_KEY } from "../../middlewares";
 import { CreateRole, GetRoles } from "./types";
